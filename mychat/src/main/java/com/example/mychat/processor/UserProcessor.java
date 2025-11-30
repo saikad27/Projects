9deca9b524377d048ff5ed44f939b7e7b6153458
saikad27 <sairajkad501@icloud.com>
2025-11-30 15:38:30 +0530

    User search updated along with login page design

--- mychat/src/main/java/com/example/mychat/processor/UserProcessor.java
+++ mychat/src/main/java/com/example/mychat/processor/UserProcessor.java
@@ -3,20 +3,24 @@ package com.example.mychat.processor;
 
 import com.example.mychat.model.UserDetail;
 import com.example.mychat.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserProcessor {
     private final UserRepository userRepository;
-    UserProcessor(UserRepository userRepository){
+    private final BCryptPasswordEncoder passwordEncoder;
+    UserProcessor(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void processUser(String email, String username,String password){
         UserDetail userDetail = new UserDetail();
         userDetail.setUsername(username);
         userDetail.setEmail(email);
-        userDetail.setPassword(password);
+        String encodedPassword = passwordEncoder.encode(password);
+        userDetail.setPassword(encodedPassword);
         userRepository.save(userDetail);
     }
 }
